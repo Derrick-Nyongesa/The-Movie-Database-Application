430b0b5eadfbbfc7dceab7225a0f428cc032bd81
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-04-14 14:03:17 +0300

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadCrumbComponent } from './components/bread-crumb/bread-crumb.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroImageComponent } from './components/hero-image/hero-image.component';
+import { MovieInfoComponent } from './components/movie-info/movie-info.component';
+import { MovieInfoBarComponent } from './components/movie-info-bar/movie-info-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ActorsComponent } from './components/actors/actors.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BreadCrumbComponent,
+    NavbarComponent,
+    HeroImageComponent,
+    MovieInfoComponent,
+    MovieInfoBarComponent,
+    HomeComponent,
+    ActorsComponent,
+    NotFoundComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/movie-info-bar/movie-info-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-info-bar',
+  templateUrl: './movie-info-bar.component.html',
+  styleUrls: ['./movie-info-bar.component.css']
+})
+export class MovieInfoBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
