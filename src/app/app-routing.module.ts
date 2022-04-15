e90a841c5fea006c1928c2dc2990e78e4ab7ff3a
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-04-15 10:20:40 +0300

    Movie component sketch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'movie/:id', component: MovieComponent },
 ];
 
 @NgModule({
