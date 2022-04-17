4e65aed8ce81a69b0aca9e3a0fdb9df4bd0972cb
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2022-04-17 12:33:19 +0300

    added a list of trending movies

--- src/app/components/trending-movies/trending-movies.component.ts
+++ src/app/components/trending-movies/trending-movies.component.ts
@@ -7,7 +7,37 @@ import { MoviesService } from 'src/app/services/movies.service';
   styleUrls: ['./trending-movies.component.css'],
 })
 export class TrendingMoviesComponent implements OnInit {
+  all_movies: any;
+  movies: any;
+  total_results: any;
+
+  p: number = 1;
+  itemsPerPage = 18;
+
+  searchStr: string;
+  searchRes: string;
+  search_all: any;
+
   constructor(private http: MoviesService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getMovie(1);
+  }
+
+  getMovie(page: number) {
+    return this.http.getNowPlaying(page).subscribe(
+      (res) => {
+        this.movies = res.results;
+        this.all_movies = res.total_results;
+      },
+      (error) => console.log(error)
+    );
+  }
+
+  searchMovies(page: number) {
+    this.http.searchMovies(this.searchStr, page).subscribe((res) => {
+      this.searchRes = res.results;
+      this.search_all = res.total_results;
+    });
+  }
 }
